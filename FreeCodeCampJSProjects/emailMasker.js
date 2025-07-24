a3e77cb955735340d74f8aa83143edf879fec937
Brian-A-A <155802828+Brian-A-A@users.noreply.github.com>
2025-07-25 01:41:54 +0800

    Update emailMasker.js

--- FreeCodeCampJSProjects/emailMasker.js
+++ FreeCodeCampJSProjects/emailMasker.js
@@ -51,3 +51,65 @@ function truncateString (word, limitNum){
 console.log(truncateString(word, limitNum));
 console.log(truncateString("A-", 1));
 // Truncate a String Algorithm End
+
+// Quiz Game Start
+let questions = [
+  {
+    category: "Entertainment",
+    question: "Who sang the title song for the latest Bond film, No Time to Die?",
+    choices:["Adele","Sam Smith","Billie Eilish"],
+    answer:"Billie Eilish"
+  },
+  {
+    category: "Geography",
+    question: "Which flies a green, white, and orange (in that order) tricolor flag? ",
+    choices:["Ireland","Ivory Coast","Italy"],
+    answer:"Ireland"
+  },
+  {
+    category: "Gadgets",
+    question: "What company makes the Xperia model of smartphone?",
+    choices:["Samsung","Sony","Nokia"],
+    answer:"Sony"
+  },
+  {
+    category: "Geography",
+    question: "Which city is home to the Brandenburg Gate?",
+    choices:["Vienna","Zurich","Berlin"],
+    answer:"Berlin"
+  },
+  {
+    category: "Science",
+    question: "Which of the following is NOT a fruit?",
+    choices:["Rhubarb","Tomatoes","Avocados"],
+    answer:"Rhubarb"
+  }
+];
+
+function getRandomQuestion(question){
+  const random = Math.floor(Math.random() * question.length);
+return random, question[random];
+}
+
+function getRandomComputerChoice(choices){
+  const random = Math.floor(Math.random() * choices.length);
+return random, choices[random];
+}
+
+function getResults(question, answer){
+  if(question.answer === answer){
+    return "The computer's choice is correct!";
+  }
+  else{
+    return "The computer's choice is wrong. The correct answer is: " + question.answer;
+  }
+}
+
+let questionForToday = (getRandomQuestion(questions));
+
+let inQuestion = questionForToday.question;
+
+let answerForToday = (getRandomComputerChoice(questionForToday.choices));
+
+console.log(getResults(questionForToday, answerForToday));
+// Quiz Game End
